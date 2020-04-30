76396b3e5ecc211efdc5529cd65efcdcc9bd67cc
291554-pw <01131742@pw.edu.pl>
2020-04-30 18:37:18 +0200

    get_shot_coord()

--- client.py
+++ client.py
@@ -9,14 +9,10 @@ class Client:
         self.player_no = None
 
     def connect(self):
-        """Try connecting to the server."""
-        try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-            return True
-        except socket.error as exception:
-            print("socket.error exception:", exception)
-            return False
+        """Connect to the server."""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        print("Connected to:", self.host, "at", self.port)
 
     def player_init(self):
         """Set player nick and receive player number."""
@@ -30,11 +26,11 @@ class Client:
             turn = self.socket.recv(1024).decode('utf-8')
             if turn == self.player_no:
                 print('Your turn!')
-                self.socket.sendall(input().encode('utf-8'))
+                self.socket.sendall(get_shot_coord())
 
 
 def get_server_info():
-    """Get server hostname and port from user."""
+    """Get server hostname and port from the user."""
     host = input("Enter server hostname or IP address (enter nothing for 127.0.0.1): ")
     if not host:
         host = '127.0.0.1'
@@ -42,6 +38,19 @@ def get_server_info():
     if not port:
         port = 12345
     return host, int(port)
+
+
+def get_shot_coord():
+    """Get shot coordinates from the user."""
+    correct = False
+    shot = None
+    while not correct:
+        shot = input("Enter shot coordinates in a form \"x y\": ")
+        if len(shot.split(" ")) != 2 or not all(coord.isdigit() for coord in shot.split(" ")):
+            print("Wrong shot coordinates.")
+        else:
+            correct = True
+    return shot.encode('utf-8')
 
 
 if __name__ == "__main__":
@@ -53,5 +62,7 @@ if __name__ == "__main__":
         client.socket.close()
     except KeyboardInterrupt:
         client.socket.close()
+    except socket.error as exception:
+        print("Connection error:", exception)
 
 
