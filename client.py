4c740138b3dc2a723d75bb22685fa2ab4523e98a
291554-pw <01131742@pw.edu.pl>
2020-04-30 17:36:46 +0200

    some client functionality

--- client.py
+++ client.py
@@ -1,26 +1,57 @@
 import socket
-import threading as thread
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 12340       # The port used by the server
 
-# def get_message(sock):
-#     while True:
-#         data = sock[0].recv(1024)
-#         if not data:
-#             continue
-#         print(data.decode('utf-8'))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    # thread.Thread(target=get_message, args=[s]).start()
-    nick = bytes(input('Your nickname: '), 'utf-8')
-    s.sendall(nick)
-    player_no = s.recv(1024).decode('utf-8')
-    while True:
-        turn = s.recv(1024).decode('utf-8')
-        if turn == player_no:
-            print('Your turn!')
-            s.sendall(input().encode('utf-8'))
-        
+class Client:
+    def __init__(self):
+        self.host, self.port = get_server_info()
+        self.connected = False
+        self.socket = None
+        self.player_no = None
+
+    def connect(self):
+        """Try connecting to the server."""
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.host, self.port))
+            return True
+        except socket.error as exception:
+            print("socket.error exception:", exception)
+            return False
+
+    def player_init(self):
+        """Set player nick and receive player number."""
+        nick = bytes(input('Your nickname: '), 'utf-8')
+        self.socket.sendall(nick)
+        self.player_no = self.socket.recv(1024).decode('utf-8')
+
+    def loop(self):
+        """<TODO>"""
+        while True:
+            turn = self.socket.recv(1024).decode('utf-8')
+            if turn == self.player_no:
+                print('Your turn!')
+                self.socket.sendall(input().encode('utf-8'))
+
+
+def get_server_info():
+    """Get server hostname and port from user."""
+    host = input("Enter server hostname or IP address (enter nothing for 127.0.0.1): ")
+    if not host:
+        host = '127.0.0.1'
+    port = input("Enter port (enter nothing for 12345): ")
+    if not port:
+        port = 12345
+    return host, int(port)
+
+
+if __name__ == "__main__":
+    try:
+        client = Client()
+        client.connect()
+        client.player_init()
+        client.loop()
+        client.socket.close()
+    except KeyboardInterrupt:
+        client.socket.close()
 
 
