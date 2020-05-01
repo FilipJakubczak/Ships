98746e7d5d12f5e51f6d5d4626b800b6a839886b
291554-pw <01131742@pw.edu.pl>
2020-05-01 21:50:05 +0200

    Client class

--- client.py
+++ client.py
@@ -4,73 +4,104 @@ import json
 import sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 12341       # The port used by the server
+PORT = 12340       # The port used by the server
 
-turn = None
 
-def listener(sock):
-    global turn
-    while True:
+class Client:
+    def __init__(self, host, port):
+        self.host, self.port = host, port
+        self.sock = None
+        self.name = None
+        self.game_finished = False
+        self.turn = None
+
+    def start(self):
+        """Try to join the game and play."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            self.sock = s
+            self.connect()
+            self.set_name()
+            self.send_msg({'type': 'connect', 'data': self.name})
+            listener = thread.Thread(target=self.listener)
+            listener.start()
+            listener.join()
+
+    def connect(self):
+        """Connect to the server."""
         try:
-            data = sock.recv(2048)
-        except:
-            print('Connection to server lost.')
-            return
-        if data == '':
-            print('Connection to server lost.')
-            return
-        msg = json.loads(str(data.decode('utf-8')))
-        msgtype = msg['type']
-        msgdata = msg['data']
+            self.sock.connect((self.host, self.port))
+        except Exception as ex:
+            print(ex)
+            print('Could not connect to server.')
+            sys.exit(1)
+        print('Connected to server.')
+
+    def set_name(self):
+        """Set player nickname."""
+        nick = input('Your nickname: ')
+        self.name = nick
+
+    def send_msg(self, msg):
+        """Send a message to the server."""
+        msg = json.dumps(msg)
+        self.sock.send(msg.encode('utf-8'))
+
+    def listener(self):
+        """A listener that responds to messages from the server."""
+        while not self.game_finished:
+            try:
+                data = self.sock.recv(2048)
+            except:
+                print('Connection to server lost.')
+                return
+            if data == '':
+                print('Connection to server lost.')
+                return
+            msg = json.loads(str(data.decode('utf-8')))
+            self.process_msg(msg)
+        self.send_msg({'type': 'disconnect', 'data': None})
+
+    def process_msg(self, msg):
+        """Process the message from the server."""
+        msgtype, msgdata = msg['type'], msg['data']
         if msgtype == 'playerlist':
             print('Players:', msg['data'])
         elif msgtype == 'begin_battle':
-            turn = msgdata['turn']
-            print('Turn:', msgdata['turn'])
-            if turn == name:
-                attack(sock)
+            self.turn = msgdata['turn']
+            print('Turn:', self.turn)
+            if self.turn == self.name:
+                self.attack()
         elif msgtype == 'attack_result':
-            if msgdata['result']:
-                print('Hit!')
-            else:
-                print('Miss!')
-            turn = msgdata['turn']
-            if turn == name:
-                attack(sock)
+            print('Hit!' if msgdata['result'] else 'Miss!')
+            self.turn = msgdata['turn']
+            if self.turn == self.name:
+                self.attack()
             print('Turn:', msgdata['turn'])
         elif msgtype == 'verdict':
             if msgdata['result'] == 'win':
                 print("You win!")
             elif msgdata['result'] == 'lose':
                 print("You lose!")
-            break
-    
-    send_msg(sock, {'type': 'disconnect', 'data': None})
+            self.game_finished = True
 
-def send_msg(sock, msg):
-    msg = json.dumps(msg)
-    sock.send(msg.encode('utf-8'))
-    
-def attack(sock):
-    x, y = input('Attack position:').split(' ')
-    send_msg(s, {'type': 'attack', 'data': (x, y)})
-
-name = input('Your name: ')
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    try:
-        s.connect((HOST, PORT))
-    except Exception as e:
-        print(e)
-        print('Could not connect to server.')
-        sys.exit(1)
-    print('Connected to server.')
-    send_msg(s, {'type': 'connect', 'data': name})
-    listener = thread.Thread(target=listener, args=(s,))
-    listener.start()
-    listener.join()
-    
-    
-    
-        
+    def attack(self):
+        """Attack the opponent at given position."""
+        x, y = get_shot_coord().split(' ')
+        self.send_msg({'type': 'attack', 'data': (x, y)})
 
 
+def get_shot_coord():
+    """Get shot coordinates from the user."""
+    correct = False
+    shot = None
+    while not correct:
+        shot = input('Enter shot coordinates in a form "x y": ')
+        if len(shot.split(" ")) != 2 or not all(coord.isdigit() for coord in shot.split(" ")):
+            print('Wrong shot coordinates.')
+        else:
+            correct = True
+    return shot
+
+
+client = Client(HOST, PORT)
+client.start()
